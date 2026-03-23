53b2c3eac5f9cfd131184ee19e0242c4519f0a9a
廖小白 <Goatman914@gmail.com>
2026-03-23 10:49:18 +0800

    Template of Unit

---
+++ Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    // 属性
+    protected float hp;
+    protected float maxHp;
+    protected float attack;
+    protected float moveSpeed;
+
+    // 移动
+    public void Move(Vector2 direction)
+    {
+        Debug.Log("通用移动");
+    }
+
+    // 攻击
+    public virtual void Attack() { }
+}
+
+public class Archer : Unit
+{
+    public override void Attack()
+    {
+        Debug.Log("实际的攻击逻辑-弓箭手");
+    }
+}
+
+public class Goblin : Unit
+{
+    public override void Attack()
+    {
+        Debug.Log("实际的攻击逻辑-哥布林");
+    }
+}
